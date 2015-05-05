70ad1814df3d5b5529d039e7cc73ffcd3c34af51
kgryte <kgryte@gmail.com>
2015-05-05 16:51:01 -0700

    [UPDATE] dotfiles. accessor and copy option. add build envs. README. [TESTS] arg validation. copy and accessor.

--- examples/index.js
+++ examples/index.js
@@ -1,12 +1,10 @@
+'use strict';
+
 var erfinv = require( './../lib' );
 
-// Simulate some data...
 var data = new Array( 100 );
-
 for ( var i = 0; i < data.length; i++ ) {
 	data[ i ] = ( Math.random() - 0.5 ) * 2;
 }
-
-// Evaluate the inverse error function for each datum:
 console.log( erfinv( data ) );
 // returns [...]
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+/* global require, describe, it */
+'use strict';
 
 // MODULES //
 
@@ -17,7 +19,6 @@ var expect = chai.expect,
 // TESTS //
 
 describe( 'compute-erfinv', function tests() {
-	'use strict';
 
 	it( 'should export a function', function test() {
 		expect( erfinv ).to.be.a( 'function' );
@@ -25,18 +26,18 @@ describe( 'compute-erfinv', function tests() {
 
 	it( 'should throw an error if not provided a numeric value or an array', function test() {
 		var values = [
-				'5',
-				true,
-				undefined,
-				null,
-				{},
-				function(){}
-			];
+			'5',
+			new Number( 0.5 ),
+			true,
+			undefined,
+			null,
+			{},
+			function(){}
+		];
 
 		for ( var i = 0; i < values.length; i++ ) {
 			expect( badValue( values[i] ) ).to.throw( TypeError );
 		}
-
 		function badValue( value ) {
 			return function() {
 				erfinv( value );
@@ -44,25 +45,127 @@ describe( 'compute-erfinv', function tests() {
 		}
 	});
 
-	it( 'should throw an error if a data array contains non-numeric values', function test() {
+	it( 'should throw an error if provided an options argument which is not an object', function test() {
 		var values = [
-				'5',
-				true,
-				undefined,
-				null,
-				[],
-				{},
-				function(){}
-			];
+			'5',
+			5,
+			true,
+			undefined,
+			null,
+			NaN,
+			[],
+			function(){}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				erfinv( [1,2,3], value );
+			};
+		}
+	});
+
+	it( 'should throw an error if provided a copy option which is not a boolean primitive', function test() {
+		var values = [
+			'5',
+			5,
+			new Boolean( true ),
+			undefined,
+			null,
+			NaN,
+			[],
+			{},
+			function(){}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				erfinv( [1,2,3], {
+					'copy': value
+				});
+			};
+		}
+	});
+
+	it( 'should throw an error if provided an accessor option which is not a function', function test() {
+		var values = [
+			'5',
+			5,
+			true,
+			undefined,
+			null,
+			NaN,
+			[],
+			{}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				erfinv( [1,2,3], {
+					'accessor': value
+				});
+			};
+		}
+	});
+
+	it( 'should throw an error if an input array contains non-numeric values (if not provided an accessor)', function test() {
+		var values = [
+			'5',
+			new Number( 0.5 ),
+			true,
+			undefined,
+			null,
+			[],
+			{},
+			function(){}
+		];
 
 		for ( var i = 0; i < values.length; i++ ) {
 			expect( badValue( [ values[i] ] ) ).to.throw( TypeError );
 		}
-
 		function badValue( value ) {
 			return function() {
 				erfinv( value );
 			};
+		}
+	});
+
+	it( 'should throw an error if an accessed array value is not numeric', function test() {
+		var values = [
+			'5',
+			new Number( 5 ),
+			true,
+			undefined,
+			null,
+			NaN,
+			[],
+			{},
+			function(){}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( [ values[i] ] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				var arr = [
+					{'x': value}
+				];
+				erfinv( arr, {
+					'accessor': getValue
+				});
+			};
+		}
+		function getValue( d ) {
+			return d.x;
 		}
 	});
 
@@ -74,19 +177,18 @@ describe( 'compute-erfinv', function tests() {
 
 	it( 'should throw an error if provided a value not on the interval [-1,1]', function test() {
 		var values = [
-				-2,
-				2
-			];
+			-2,
+			2
+		];
 
-			for ( var i = 0; i < values.length; i++ ) {
-				expect( badValue( values[i] ) ).to.throw( Error );
-			}
-
-			function badValue( value ) {
-				return function() {
-					erfinv( value );
-				};
-			}
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( Error );
+		}
+		function badValue( value ) {
+			return function() {
+				erfinv( value );
+			};
+		}
 	});
 
 	it( 'should return positive infinity if provided 1', function test() {
@@ -110,17 +212,44 @@ describe( 'compute-erfinv', function tests() {
 	});
 
 	it( 'should return an array of numbers if provided an array', function test() {
-		var values = [
-				0.2,
-				0.3
-			],
-			val;
+		var values, out;
 
+		values = [
+			0.2,
+			0.3
+		];
+
+		out = erfinv( values );
+		assert.isArray( out );
 		for ( var i = 0; i < values.length; i++ ) {
-			val = erfinv( [ values[ i ] ] );
-			assert.isArray( val );
-			assert.isNumber( val[ 0 ] );
+			assert.isNumber( out[ i ] );
 		}
+	});
+
+	it( 'should not mutate the input array by default', function test() {
+		var values, out;
+
+		values = [
+			0.2,
+			0.3
+		];
+
+		out = erfinv( values );
+		assert.ok( out !== values );
+	});
+
+	it( 'should mutate an input array if the `copy` option is `false`', function test() {
+		var values, out;
+
+		values = [
+			0.2,
+			0.3
+		];
+
+		out = erfinv( values, {
+			'copy': false
+		});
+		assert.ok( out === values );
 	});
 
 	it( 'should evaluate the inverse error function', function test() {
@@ -161,6 +290,52 @@ describe( 'compute-erfinv', function tests() {
 
 		for ( var i = 0; i < actual.length; i++ ) {
 			assert.closeTo( actual[ i ], expected[ i ], 1e-4 );
+		}
+	});
+
+	it( 'should evaluate the inverse error function using an accessor function', function test() {
+		var values, expected, actual;
+
+		values = [
+			[1,0.25],
+			[2,-0.25],
+			[3,0.6],
+			[4,-0.6],
+			[5,0.8],
+			[6,-0.8],
+			[7,0.999],
+			[8,-0.999],
+			[9,0.9999],
+			[10,-0.9999],
+			[11,9.999999999999999e-1],
+			[12,-9.999999999999999e-1]
+		];
+
+		// Evaluated on Wolfram Alpha and Octave:
+		expected = [
+			0.225312,
+			-0.225312,
+			0.595116,
+			-0.595116,
+			0.906194,
+			-0.906194,
+			2.32675,
+			-2.32675,
+			2.75106,
+			-2.75106,
+			5.8636, // Octave
+			-5.8636 // Octave
+		];
+
+		actual = erfinv( values, {
+			'accessor': getValue
+		});
+
+		for ( var i = 0; i < actual.length; i++ ) {
+			assert.closeTo( actual[ i ], expected[ i ], 1e-4 );
+		}
+		function getValue( d ) {
+			return d[ 1 ];
 		}
 	});
 
