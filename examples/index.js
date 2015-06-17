1f244fa2dae08a78d289994ecda0df262ac5bb33
Planeshifter <pburckhardt@outlook.com>
2015-06-17 13:54:52 +0200

    [UPDATE] examples

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,83 @@
 'use strict';
 
-var erfinv = require( './../lib' );
+var matrix = require( 'dstructs-matrix' ),
+	erfinv = require( './../lib' );
 
-var data = new Array( 100 );
-for ( var i = 0; i < data.length; i++ ) {
+var data,
+	mat,
+	out,
+	tmp,
+	i;
+
+// ----
+// Plain arrays...
+data = new Array( 100 );
+for ( i = 0; i < data.length; i++ ) {
 	data[ i ] = ( Math.random() - 0.5 ) * 2;
 }
-console.log( erfinv( data ) );
-// returns [...]
+out = erfinv( data );
+console.log( 'Arrays: %s\n', out );
+
+
+// ----
+// Object arrays (accessors)...
+function getValue( d ) {
+	return d.x;
+}
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': data[ i ]
+	};
+}
+out = erfinv( data, {
+	'accessor': getValue
+});
+console.log( 'Accessors: %s\n', out );
+
+
+// ----
+// Deep set arrays...
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = {
+		'x': [ i, data[ i ].x ]
+	};
+}
+out = erfinv( data, {
+	'path': 'x/1',
+	'sep': '/'
+});
+console.log( 'Deepset:' );
+console.dir( out );
+console.log( '\n' );
+
+
+// ----
+// Typed arrays...
+data = new Int32Array( 10 );
+for ( i = 0; i < data.length; i++ ) {
+	data[ i ] = ( Math.random() - 0.5 ) * 2;
+}
+tmp = erfinv( data );
+out = '';
+for ( i = 0; i < data.length; i++ ) {
+	out += tmp[ i ];
+	if ( i < data.length-1 ) {
+		out += ',';
+	}
+}
+console.log( 'Typed arrays: %s\n', out );
+
+
+// ----
+// Matrices...
+mat = matrix( data, [5,2], 'int32' );
+out = erfinv( mat );
+console.log( 'Matrix: %s\n', out.toString() );
+
+
+// ----
+// Matrices (custom output data type)...
+out = erfinv( mat, {
+	'dtype': 'uint8'
+});
+console.log( 'Matrix (%s): %s\n', out.dtype, out.toString() );
